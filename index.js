882c7a0c203b36f208e94444a3858b19ec5c8b44
joshuamicheletti <j.micheletti@campus.uniurb.it>
2021-03-02 16:38:57 +0100

    update

--- index.js
+++ index.js
@@ -1,66 +1,98 @@
-// const express = require('express')
-// const path = require('path')
-// const PORT = process.env.PORT || 5000
-
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
 
-// formattazione richieste in json
 app.use(express.json());
 
-const got = require("got");
+const data = new Map();
+data.set(1, {name: "Mario", surname: "Rossi"});
+data.set(2, {name: "Luigi", surname: "Verdi"});
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.get('/people', (req, resp) => {
+  if (!req.accepts('application/json')) {
+    resp.sendStatus(406);
+    return;
+  }
+  
+  const output = Array.from(data, ([key, value]) => {
+    return {
+      id: key,
+      name: value.name,
+      surname: value.surname
+    };
+  });
+  resp.json(output);
+});
+
+app.get('/people/:id', (req, resp) => {
+  const id = Number.parseInt(req.params.id);
+  
+  if (Number.isNaN(id)) {
+    resp.sendStatus(400);
+    return;
+  }
+  if (!data.has(id)) {
+    resp.sendStatus(404);
+    return;
+  }
+  
+  const person = data.get(id);
+  
+  resp.format({
+    'text/plain': () => {
+      resp.send(person.name + ' ' + person.surname);
+    },
+    'text/html': () => {
+      resp.send('<html><body><p>Il signor <b>' + person.name + ' ' + person.surname + '</b></p></body></html>');
+    },
+    'application/json': () => {
+      //resp.send(JSON.stringify(person));
+      resp.json(person);
+    },
+    defaul: () => {
+      resp.sendStatus(406);
+    }
+  });
+});
+
+app.post('/people', (req, resp) => {
+  const input = req.body;
+  
+  if (!input.name) {
+    resp.sendStatus(422);
+    return;
+  }
+  if (!input.surname) {
+    resp.sendStatus(422);
+    return;
+  }
+  
+  const name = new String(input.name);
+  const surname = new String(input.surname);
+  
+  const newId = data.size + 1;
+  data.set(newId, {name: name, surname: surname});
+  
+  resp.status(201).type('application/json').send(JSON.stringify({id: newId, name: name, surname: surname}));
+});
+
+app.delete('/people/:id', (req, resp) => {
+  const id = Number.parseInt(req.params.id);
+  
+  if (Number.isNaN(id)) {
+    resp.sendStatus(400);
+    return;
+  }
+  if (!data.has(id)) {
+    resp.sendStatus(404);
+    return;
+  }
+  
+  data.delete(id);
+  resp.sendStatus(200);
+});
+
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
-});
-
-app.get("/info", async (req, resp) => {
-  const bot = await got("https://api.telegram.org/bot1500961059:AAGKH7a-RHTiBW-TlpbeClXC4PKeal61das/getMe");
-  console.log(bot.body);
-  resp.sendStatus(200);
-});
-
-var testCounter = 0;
-
-var user = "";
-var text = "";
-var requestBody;
-var totalMessage = "";
-
-app.post("/telegram", (req, resp) => {
-  console.log("Messaggio in arrivo!");
-  requestBody = req.body;
-  user = req.body.message.from.first_name;
-  text = req.body.message.text;
-
-  totalMessage = totalMessage + user + ": " + text + "\r";
-
-  resp.status(200).send("Messaggio in arrivo!").end();
-  testCounter++;
-});
-
-app.get("/telegramTest", (req, resp) => {
-  // resp.status(200).send(testCounter.toString()).end();
-  // resp.status(200).send(JSON.stringify(requestBody)).end();
-  resp.status(200).send(totalMessage).end();
-  // requestBody.message.text;
-});
-
-app.get("/", (req, resp) => {
-  resp.status(200).type("text/html").send("<b>Good morning everyone :D</b>").end();
 });

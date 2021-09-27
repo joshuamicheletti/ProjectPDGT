75a9ae542d460e88e7639aa4bdf134e123836923
Joshua Micheletti <j.micheletti@campus.uniurb.it>
2021-09-27 19:46:03 +0200

    added comments on the client

--- client/program.py
+++ client/program.py
@@ -121,8 +121,10 @@ def keyboardEventHandler(keyboard_event):
     # print("Name:", keyboard_event.name)
     # print("Scan code:", keyboard_event.scan_code)
     # print("Time:", keyboard_event.time)
+    # check if the program is listening for a key
     if pressed == False:
       if keyboard_event.scan_code == 103 and operatingSystem == "linux" or keyboard_event.scan_code == 72 and operatingSystem == "windows": #up
+        # go to the next selected file or server
         if selectedFile > 0:
           selectedFile -= 1
 
@@ -131,6 +133,7 @@ def keyboardEventHandler(keyboard_event):
         pressed = True
 
       if keyboard_event.scan_code == 108 and operatingSystem == "linux" or keyboard_event.scan_code == 80 and operatingSystem == "windows": #down
+        # go to the previous file or server
         if selectedFile < len(filesList) - 1:
           selectedFile += 1
 
@@ -139,6 +142,7 @@ def keyboardEventHandler(keyboard_event):
         pressed = True
 
       if keyboard_event.scan_code == 105 and operatingSystem == "linux" or keyboard_event.scan_code == 75 and operatingSystem == "windows": #left
+        # go to the previous command
         if selectedCommand > 0:
           selectedCommand -= 1
 
@@ -147,6 +151,7 @@ def keyboardEventHandler(keyboard_event):
         pressed = True
 
       if keyboard_event.scan_code == 106 and operatingSystem == "linux" or keyboard_event.scan_code == 77 and operatingSystem == "windows": #right
+        # go to the next command
         if selectedCommand < len(commands) - 1:
           selectedCommand += 1
 
@@ -154,6 +159,7 @@ def keyboardEventHandler(keyboard_event):
           selectedLoginCommand += 1
         pressed = True
 
+      # check for enter, used to select any option
       if keyboard_event.scan_code == 28:
         enter = True
         pressed = True
@@ -165,21 +171,29 @@ def login(usernameL, passwordL):
   global username
   global password
 
+  # try to encode the username and password in base64, if username and password exist, then
   if encodeBase64(usernameL, passwordL):
-
+    # set the encoded string as an auth header
     headers = {
       'Authorization': 'Basic ' + encodeBase64(usernameL, passwordL)
     }
 
+    # make an HTTP GET request to the webapi /secret
     r = requests.get(url + '/secret', headers = headers)
 
+    # if the program returns 200 OK
     if r.status_code == 200:
+      # set the current username as the one used for the request
       username = usernameL
+      # set the current password as the one used for the request
       password = passwordL
+      # wipe the server message
       serverMessage = ""
       return True
-      
+
+    # if the request returns an error 
     else:
+      # print the error as the server message
       serverMessage = r.text
       return False
 
@@ -191,9 +205,12 @@ def loginCookie(usernameL, passwordL):
   global username
   global password
 
+  # make an HTTP GET request to /login to login and get a cookie
   r = requests.post(url + '/login?username=' + usernameL + '&password=' + passwordL)
 
+  # if the response is 200 OK
   if r.status_code == 200:
+    # create or write a cookie.txt file with the cookie received from the response
     try:
       cookieFile = open("cookie.txt", "x")
       cookieFile.write(r.cookies["auth"])
@@ -203,12 +220,17 @@ def loginCookie(usernameL, passwordL):
       cookieFile.write(r.cookies["auth"])
       cookieFile.close()
 
+    # wipe the server message
     serverMessage = ""
+    # set the current username to the one used in the request to login
     username = usernameL
+    # set the current password to the one used in the request to login
     password = passwordL
     return True
     
+  # if the response is anything else
   else:
+    # show the error in the server message
     serverMessage = r.text
     return False
 
@@ -216,38 +238,57 @@ def loginCookie(usernameL, passwordL):
 def loginWithoutCredentials():
   global username
   
+  # if there is a cookie to read
   if (readCookie()):
+    # set the cookie in the header
     headers = {
       'Cookie': 'auth=' + readCookie()
     }
 
+    # send an HTTP GET request to /secret to login through cookie
     r = requests.get(url + '/secret', headers = headers)
     
+    # if the response is 200 OK
     if r.status_code == 200:
+      # set the current username to the response message
       username = r.text
       return True 
-      
-  return False
 
+    # if the response is anything else
+    else:
+      # set the error as server message to be displayed
+      serverMessage = r.text;
+      return False
+    
 # function to register a new user
 def register(usernameL, passwordL):
   global serverMessage
   global username
   global password
 
+  # if username and password exist and can be encoded in base64
   if encodeBase64(usernameL, passwordL):
+    # set the auth header to the encoded string containing the username and password
     headers = {
       'Authorization': 'Basic ' + encodeBase64(usernameL, passwordL)
     }
 
+    # make an HTTP POST request to /users
     r = requests.post(url + '/users', headers = headers)
 
+    # if the response status is 200 OK
     if r.status_code == 200:
+      # set the current username as the username used in the request
       username = usernameL
+      # set the current password as the password used in the request
       password = passwordL
+      # wipe the server message
       serverMessage = ""
       return True
+
+    # if the response is anything else
     else:
+      # show the error as the server message
       serverMessage = r.text
       return False
   
@@ -257,17 +298,25 @@ def register(usernameL, passwordL):
 def deleteUser(usernameL, passwordL):
   global serverMessage
 
+  # if the username and password exist and can be encoded base64
   if encodeBase64(usernameL, passwordL):
+    # set the auth header with the username and password encoded in base64
     headers = {
       'Authorization': 'Basic ' + encodeBase64(usernameL, passwordL)
     }
 
+    # make an HTTP DELETE request to /users
     r = requests.delete(url + '/users', headers = headers)
 
+    # if the response is 200 OK
     if r.status_code == 200:
+      # notify the success as the server message
       serverMessage = r.text
       return True
+
+    # if the response is anything else
     else:
+      # notify the error through the server message
       serverMessage = r.text
       return False
   
@@ -557,6 +606,11 @@ def downloadFile(fileName):
 
   downloadPath = "./download/"
 
+  try:
+    os.mkdir(downloadPath)
+  except IOError:
+    pass
+
   if readCookie():
     headers = {
       'Cookie': 'auth=' + readCookie()
@@ -706,77 +760,116 @@ def main():
 
       # login with cookie
       elif selectedLoginCommand == 1 and enter == True:
+        # clean the previous server message
         serverMessage = ""
+        # flush the inputs buffer before getting user inputs
         flushInput()
+        # get the username from the user
         usernameL = input("\nUsername: ")
         print()
+        # get the password from the user without showing it
         passwordL = stdiomask.getpass(mask='*')
+        # check if the login worked
         if loginCookie(usernameL, passwordL):
           loggedIn = True
+        # set enter to false to listen for a new command
         enter = False
 
       # register
       elif selectedLoginCommand == 2 and enter == True:
+        # clean the previous server message
         serverMessage = ""
+        # flush the inputs buffer before getting user inputs
         flushInput()
+        # get the username from the user
         usernameL = input("\nUsername: ")
         print()
+        # get the password from the user without showing it
         passwordL = stdiomask.getpass(mask='*')
+        # check if the registration worked
         if register(usernameL, passwordL):
           loggedIn = True
+        # set enter to false to listen for a new command
         enter = False
 
       # delete 
       elif selectedLoginCommand == 3 and enter == True:
+        # clean the previous server message
         serverMessage = ""
+        # flush the inputs buffer before getting user inputs
         flushInput()
+        # get the username from the user
         usernameL = input("\nUsername: ")
         print()
+        # get the password from the user without showing it
         passwordL = stdiomask.getpass(mask='*')
+        # delete the specified user if available
         deleteUser(usernameL, passwordL)
         enter = False
 
       # close
       elif selectedLoginCommand == 4 and enter == True:
+        # set the variable running to False to terminate the program
         running = False
+        # flush the input to prevent any inputs within the program to affect the father program
         flushInput()
 
+      # clear the screen to set it ready for the next draw call
       clear()
 
     # SERVER LOGIN
     elif not serverLoggedIn:
+      # print the current logged in user
       print("User: " + username)
-
+      # print all the available servers and the server options on the screen
       printServers()
 
+      # keyboard event polling
       while not pressed and not enter:
         pass
       pressed = False
       
+      # if the user pressed enter (chose an option)
       if enter == True:
+        # clean the server message
         serverMessage = ""
 
         # New server
         if selectedServer == 0:
+          # clean the input buffer before a user input
           flushInput()
+          # get the new server name from the user
           serverName = input("\nServer Name: ")
+          # transform the server name into lower case (to match with the minio bucket, which only accepts lower case names)
           serverName = serverName.lower()
           print()
+          # get the new server password from the user without showing it
           serverPassword = stdiomask.getpass(mask='*')
+          # check if the new server was created correctly
           if createServer(serverName, serverPassword):
+            # notify that the user is not logged in to the newly created server
             serverLoggedIn = True
+            # notify to recheck the server list, now that a new server has been created
             upToDateServer = False
+            # set the files list to "null" to notify that when entering the server, the file list should be redownloaded
             filesList = ["null"]
+            # notify that the file list needs to be updated
             upToDate = False
+            # sets the selected file to 0 to set it to the beginning, since there won't be any files to begin with
             selectedFile = 0
+          # sets enter to false to listen for a new command
           enter = False
 
         # Logout
         elif selectedServer == len(serverList) + 1:
+          # sets enter to false to listen for a new command
           enter = False
+          # notify that the user is no longer logged in into any account
           loggedIn = False
+          # notify that the user isn't the server owner anymore
           serverOwner = False
 
+          # try to remove the cookie.txt file if it exists
           try:
             os.remove("cookie.txt")
           except IOError:
@@ -784,93 +877,146 @@ def main():
 
         # Login server
         else:
+          # clean the input buffer before a user input
           flushInput()
           print()
+          # get the server password from the user without showing it
           serverPassword = stdiomask.getpass(mask='*')
+          # set enter to false to listen for new commands
           enter = False
+          # check if the server login was succesful
           if loginServer(serverPassword):
+            # notify that the user is logged into a server
             serverLoggedIn = True
+            # set the fileslist to "null" to make it update
             filesList = ["null"]
+            # notify that the files list needs to be updated
             upToDate = False
+            # set the selected file to the first one
             selectedFile = 0
 
+      # clear the screen
       clear()
 
     # FILE MANAGEMENT
     else:
+      # if the user is the server owner
       if serverOwner:
+        # print the user in blue color
         print("User:", colored(username, 'cyan'))
+      # otherwise print the user normally
       else:
         print("User: " + username)
 
+      # print the current server
       print("\nServer: " + serverCurrent)
 
+      # print all the files within the current server
       printFiles()
 
       print()
 
+      # print all the available commands inside a server
       printCommands()
 
-      pressed = False
+      # keyboard event polling
       while not pressed and not enter:
         pass
       pressed = False
 
       # upload file
       if selectedCommand == 0 and enter == True:
+        # wipe the server message
         serverMessage = ""
+        # set enter to false to listen for a new input
         enter = False
+        # try to open a file to upload
         try:
+          # get the file path
           path = askopenfilename()
+          # upload it to the server
           uploadFile(path)
+        # if there's no file uploaded or any IOError
         except IOError:
-          print("File error")
+          # notify the user of the error
+          serverMessage = "No file selected"
 
       # download file
       elif selectedCommand == 1 and enter == True:
+        # wipe the server message
         serverMessage = ""
+        # set enter to false to listen for a new input
         enter = False
+        # download the file specified
         downloadFile(filesList[selectedFile])
 
       # delete file
       elif selectedCommand == 2 and enter == True:
+        # check if the file list isn't empty before deleting a file
         if len(filesList) == 0:
+          # if the list of files is empty, notify the user that there's no file to delete
           serverMessage = "No file to delete!"
+          # set enter to false to listen for a new input
           enter = False
         else:
+          # wipe the server message
           serverMessage = ""
+          # clean the input buffer
           flushInput()
+          # ask the user if they are sure about the choice
           choice = input("Are you sure? [Yes/No]: ")
+          # set enter to false to listen for a new input
           enter = False
+          # if the user choice was positive
           if choice == "Yes" or choice == "yes" or choice == "ye" or choice == "y":
+            # delete the selected file
             deleteFile(filesList[selectedFile])
 
       # change server
       elif selectedCommand == 3 and enter == True:
+        # wipe the server message
         serverMessage = ""
+        # set enter to false to listen for a new input
         enter = False
+        # logout from the server
         serverLoggedIn = False
+        # set the server owner to false, since we're not inside any server anymore
         serverOwner = False
         
       # delete server
       elif selectedCommand == 4 and enter == True:
+        # wipe the server message
         serverMessage = ""
+        # clean the input buffer
         flushInput()
+        # ask the user if they are sure about the choice
         choice = input("Are you sure? [Yes/No]: ")
+        # set enter to false to listen for a new input
         enter = False
+        # if the user choice was positive
         if choice == "Yes" or choice == "yes" or choice == "ye" or choice == "y":
+          # delete the server
           if deleteServer():
+            # logout of the server
             serverLoggedIn = False
+            # set the server owner to false since there's no server anymore
             serverOwner = False
+            # notify that the server list needs to be updated
             upToDateServer = False
 
       # logout
       elif selectedCommand == 5 and enter == True:
+        # wipe the server message
         serverMessage = ""
+        # set enter to false to listen for new inputs
         enter = False
+        # logout of the account
         loggedIn = False
+        # logout of the server
         serverLoggedIn = False
+        # set the server owner to false, since we're not logged in to a server anymore
         serverOwner = False
+        # try to remove the cookie file in case it's there
         try:
           os.remove("cookie.txt")
         except IOError:
@@ -878,10 +1024,14 @@ def main():
         
       # close
       elif selectedCommand == 6 and enter == True:
+        # set enter to false to listen for new inputs
         enter = False
+        # stops the program from running
         running = False
+        # clear the inputs before closing
         flushInput()
         
+      # clean the screen
       clear()
 
 

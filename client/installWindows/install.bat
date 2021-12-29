e62a2d89011df9237c0abf6bb7a68ffe84a7185e
Joshua Micheletti <j.micheletti@campus.uniurb.it>
2021-12-29 17:58:18 +0100

    added instructions in the windows install script

--- client/installWindows/install.bat
+++ client/installWindows/install.bat
@@ -4,3 +4,8 @@ pip install stdiomask
 pip install termcolor
 
 echo python program.py> ../launch.bat
+
+echo off
+echo:
+echo TO RUN THE PROGRAM, HEAD TO "ProjectPDGT/client" AND RUN "launch.bat"
+echo:
--- client/installWindows/uninstall.bat
+++ client/installWindows/uninstall.bat
@@ -1,3 +1,10 @@
+echo off
+echo:
+echo THIS WILL UNINSTALL EVERY PROGRAM AND DEPENDENCY USED IN THE PROGRAM.
+echo:
+echo IF YOU WISH TO KEEP SOME SOFTWARE, MAKE SURE YOU DECLINE THE UNINSTALL PROCESS OF THE SPECIFIC SOFTWARE YOU WISH TO KEEP
+echo:
+
 pip uninstall requests
 pip uninstall keyboard
 pip uninstall stdiomask

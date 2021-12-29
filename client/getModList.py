b794f58acca50f311f6b08b4b124da4af24d4d3f
Joshua Micheletti <j.micheletti@campus.uniurb.it>
2021-12-29 18:32:28 +0100

    removed older versions of code

--- client/getModList.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# url = "https://projectpdgt.herokuapp.com/upload"
-url = "http://localhost:2000/upload"
-
-r = requests.request("GET", url)
-
-print(r.status_code)
-print(r.text)
